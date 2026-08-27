5c6f2fcf5af54e92e3d3fb435b84e1c51cac1e3b
Samuel Padron <spadron05@gmail.com>
2026-08-27 21:42:16 +0200

    Add command execution with tests

---
+++ functions/run_python_file.py
@@ -0,0 +1,46 @@
+import os
+import subprocess
+
+
+def run_python_file(
+    working_directory: str, file_path: str, args: list[str] | None = None
+) -> str:
+    try:
+        working_abs_path = os.path.realpath(working_directory)
+        absolute_file_path = os.path.realpath(os.path.join(working_abs_path, file_path))
+        
+        valid_path = os.path.commonpath([working_abs_path, absolute_file_path]) == working_abs_path
+        
+        if not valid_path:
+            return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+        
+        if not os.path.isfile(absolute_file_path):
+            return f'Error: "{file_path}" does not exist or is not a regular file'
+        
+        if not absolute_file_path[-3:] == ".py":
+            return f'Error: "{file_path}" is not a Python file'
+        
+        command = ["python", absolute_file_path]
+        
+        if args:
+            command.extend(args)
+            
+        process = subprocess.run(command,capture_output=True, text= True, timeout=30)
+        return_code = process.returncode
+        output = ""
+        if return_code != 0:
+             output += f"Process exited with code {return_code} "
+            
+        if not process.stdout and not process.stderr:
+            output += "No output produced. "
+            
+        if process.stdout:
+            output += f"STDOUT: {process.stdout} "
+            
+        if process.stderr:
+            output += f"STDERR: {process.stderr} "
+                    
+        return output
+            
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
