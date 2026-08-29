7a464ffc91f0eb4b307042207e7226c47d0f4060
Samuel Padron <spadron05@gmail.com>
2026-08-29 19:47:39 +0200

    Add basic functions to available functions

--- call_function.py
+++ call_function.py
@@ -1,7 +1,14 @@
 from typing import cast
 from functions.get_files_info import schema_get_files_info
+from functions.get_file_content import schema_get_file_content
+from functions.write_file import schema_write_file
+from functions.run_python_file import schema_run_python_file
 from openai.types.chat import ChatCompletionToolUnionParam
+
 
 available_functions: list[ChatCompletionToolUnionParam] = [
     cast(ChatCompletionToolUnionParam, schema_get_files_info),
+    cast(ChatCompletionToolUnionParam, schema_get_file_content),
+    cast(ChatCompletionToolUnionParam, schema_write_file),
+    cast(ChatCompletionToolUnionParam, schema_run_python_file),
 ]
